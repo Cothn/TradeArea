d2318d74cd098ecc64186acd3e17d75884323e3b
Cothn <48108190+Cothn@users.noreply.github.com>
2023-06-19 22:09:51 +0300

    Created maven project, with springBoot application and liquibase migrtions
    Добавлен pom файл maven.
    Создан проект spring boot.
    Добавлены liquibase скрипты для инициализации схемы данных.

---
+++ src/main/java/com/example/tradeArea/TradeAreaApplication.java
@@ -0,0 +1,15 @@
+package com.example.tradeArea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Vsevolod Grinchick
+ */
+@SpringBootApplication
+public class TradeAreaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeAreaApplication.class, args);
+    }
+}
